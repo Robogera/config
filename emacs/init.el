a921907001705de5af1d8758d07c2d58bda5f3c0
Gera Cherdakly <robogera@gmail.com>
2026-03-06 13:35:49 +0300

    expanded emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -47,17 +47,23 @@
   :demand t
   :ensure nil
   :init
-
-  (menu-bar-mode -1) (tool-bar-mode -1) (scroll-bar-mode -1)
+  (load-theme 'modus-vivendi t)
+  (setq make-backup-files nil) ; TODO: maybe move to a directory in $HOME
+  (setq auto-save-default nil)
+  (setq-default display-line-numbers-width 4)
+  (setq scroll-conservatively 1000
+    scroll-margin 10)
+  (global-unset-key (kbd "<f10>"))
+  (setq use-dialog-box nil) 
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
   (setq inhibit-startup-message t)
+  (setq display-line-numbers-type 'relative)
   (global-display-line-numbers-mode 1)
   (set-frame-font "Terminus 12" nil t)
-
-  (setq use-dialog-box nil)
-
   (setq enable-recursive-minibuffers t)
-  (setq backup-by-copying t)
-  (setq sentence-end-double-space nil)
+  (setq sentence-end-double-space nil) 
   (setq frame-inhibit-implied-resize t) ;; useless for a tiling window manager
   (setq show-trailing-whitespace t) ;; self-explanatory
   (setq indent-tabs-mode nil) ;; no tabs
@@ -73,106 +79,55 @@
   (prefer-coding-system 'utf-8)
   (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
 
-  ; (global-set-key (kbd "<escape>") 'keyboard-escape-quit) ;; escape quits everything
+  (global-set-key (kbd "<escape>") 'keyboard-escape-quit) ;; escape quits everything
 
-  ;; less noise when compiling elisp
   (setq byte-compile-warnings '(not free-vars unresolved noruntime lexical make-local))
   (setq native-comp-async-report-warnings-errors nil)
   (setq load-prefer-newer t)
 
   (show-paren-mode t))
 
-(use-package meow
- :ensure t
- :demand t
- :init
-    (require 'meow)
-  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
-  (meow-motion-define-key
-   '("j" . meow-next)
-   '("k" . meow-prev)
-   '("<escape>" . ignore))
-  (meow-leader-define-key
-   ;; Use SPC (0-9) for digit arguments.
-   '("1" . meow-digit-argument)
-   '("2" . meow-digit-argument)
-   '("3" . meow-digit-argument)
-   '("4" . meow-digit-argument)
-   '("5" . meow-digit-argument)
-   '("6" . meow-digit-argument)
-   '("7" . meow-digit-argument)
-   '("8" . meow-digit-argument)
-   '("9" . meow-digit-argument)
-   '("0" . meow-digit-argument)
-   '("/" . meow-keypad-describe-key)
-   '("?" . meow-cheatsheet))
-  (meow-normal-define-key
-   '("0" . meow-expand-0)
-   '("9" . meow-expand-9)
-   '("8" . meow-expand-8)
-   '("7" . meow-expand-7)
-   '("6" . meow-expand-6)
-   '("5" . meow-expand-5)
-   '("4" . meow-expand-4)
-   '("3" . meow-expand-3)
-   '("2" . meow-expand-2)
-   '("1" . meow-expand-1)
-   '("-" . negative-argument)
-   '(";" . meow-reverse)
-   '("," . meow-inner-of-thing)
-   '("." . meow-bounds-of-thing)
-   '("[" . meow-beginning-of-thing)
-   '("]" . meow-end-of-thing)
-   '("a" . meow-append)
-   '("A" . meow-open-below)
-   '("b" . meow-back-word)
-   '("B" . meow-back-symbol)
-   '("c" . meow-change)
-   '("d" . meow-delete)
-   '("D" . meow-backward-delete)
-   '("e" . meow-next-word)
-   '("E" . meow-next-symbol)
-   '("f" . meow-find)
-   '("g" . meow-cancel-selection)
-   '("G" . meow-grab)
-   '("h" . meow-left)
-   '("H" . meow-left-expand)
-   '("i" . meow-insert)
-   '("I" . meow-open-above)
-   '("j" . meow-next)
-   '("J" . meow-next-expand)
-   '("k" . meow-prev)
-   '("K" . meow-prev-expand)
-   '("l" . meow-right)
-   '("L" . meow-right-expand)
-   '("m" . meow-join)
-   '("n" . meow-search)
-   '("o" . meow-block)
-   '("O" . meow-to-block)
-   '("p" . meow-yank)
-   '("q" . meow-quit)
-   '("Q" . meow-goto-line)
-   '("r" . meow-replace)
-   '("R" . meow-swap-grab)
-   '("s" . meow-kill)
-   '("t" . meow-till)
-   '("u" . meow-undo)
-   '("U" . meow-undo-in-selection)
-   '("v" . meow-visit)
-   '("w" . meow-mark-word)
-   '("W" . meow-mark-symbol)
-   '("x" . meow-line)
-   '("X" . meow-goto-line)
-   '("y" . meow-save)
-   '("Y" . meow-sync-grab)
-   '("z" . meow-pop-selection)
-   '("'" . repeat)
-   '("<escape>" . ignore))
-  (meow-global-mode 1))
-
 (use-package vterm
   :demand t
   :ensure t
   :init
-  (setq vterm-timer-delay 0.0))
+  (setq vterm-max-scrollback 10000)
+  (setq vterm-timer-delay 0.001))
 
+(use-package eat
+  :demand t
+  :ensure t
+  :config
+  (add-hook 'eat-exec-hook
+    (lambda (_) (eat-char-mode)))
+  (add-hook 'eshell-mode-hook  #'eat-eshell-mode)
+  (add-hook 'eshell-mode-hook  #'eat-eshell-visual-command-mode))
+
+(use-package evil
+  :demand t
+  :ensure t
+  :init
+  (require 'evil)
+  (evil-mode 1)
+  (evil-define-key '(normal) 'global (kbd "g c") 'comment-dwim)
+  (evil-define-key '(normal visual) 'global (kbd "s") 'avy-goto-word-0))
+
+(use-package avy
+  :demand t
+  :ensure t)
+  
+(use-package eshell
+  :demand t
+  :ensure nil
+  :init
+  (setq eshell-scroll-to-bottom-on-input t)
+  (setq eshell-history-size 10000)
+  (setq eshell-save-history-on-exit t)
+  (setq eshell-hist-ignoredups t))
+
+(use-package esh-module
+  :demand t
+  :ensure nil
+  :defer t
+  :config
+  (add-to-list 'eshell-modules-list 'eshell-tramp))
